2f5ac9eabfc9609bce3aa420a64f29279e626e95
Alan Coopersmith <alan.coopersmith@oracle.com>
2013-11-04 22:28:19 -0800

    Correct usage message for scanline unit option to be -u, not -s
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
    Reviewed-by: Matthieu Herrb <matthieu@herrb.eu>

--- bdftopcf.c
+++ bdftopcf.c
@@ -148,7 +148,7 @@ main(int argc, char *argv[])
                 fprintf(stderr,
                         "usage: %s [-p#] [-u#] [-m] [-l] [-M] [-L] [-t] [-i] [-o pcf file] [bdf file]\n"
                         "       where # for -p is 1, 2, 4, or 8\n"
-                        "       and   # for -s is 1, 2, or 4\n",
+                        "       and   # for -u is 1, 2, or 4\n",
                         program_name);
                 exit(1);
             }
